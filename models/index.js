26a090b35a7e264f6722265f6bdc2412ef8df0c5
Anvitha Reddy <anvithareddy@0502@gmail.com>
2026-04-13 16:45:38 +0530

    fixed sqlite for render

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -13,16 +12,13 @@ const db = {};
 let sequelize;
 
 //  DB CONNECTION
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+  logging: false
+});
+
+
 
 //  LOAD ALL MODELS AUTOMATICALLY
 fs
